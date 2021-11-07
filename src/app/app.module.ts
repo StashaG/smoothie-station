2fdad832aff645448ec7eb1847fbd12a34e6fcdb
Stacey Graham <stacey.graham1106@gmail.com>
2021-11-07 16:02:07 -0500

    added home, header, and recipes components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
